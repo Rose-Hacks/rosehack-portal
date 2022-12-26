755ef61631b52bb7579cf4da3a2f63dc0e7cd539
Divyank Shah <divyank.shah.2016@gmail.com>
2022-12-25 18:30:38 -0800

    next/font/google

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,20 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import React from "react";
+// eslint-disable-next-line camelcase
+import { Lexend, Press_Start_2P } from "@next/font/google";
+
+const lexend = Lexend({ subsets: ["latin"], variable: "--font-lexend" });
+const pixel = Press_Start_2P({
+  subsets: ["latin"],
+  variable: "--font-pixel",
+  weight: "400",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${lexend.variable} ${pixel.variable}`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,22 +5,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Lexend:wght@400;700&display=swap"
-            rel="stylesheet"
-          ></link>
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
