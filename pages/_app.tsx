53fae5bb965ff1ee7490e187c31cb8f2484050a3
Divyank Shah <divyank.shah.2016@gmail.com>
2022-12-26 04:28:33 -0800

    make bootstrap work

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 // eslint-disable-next-line camelcase
 import { Lexend, Press_Start_2P } from "@next/font/google";
 
