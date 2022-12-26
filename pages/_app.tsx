071853e6b271e61097f74c1f12be98f8c7f5d270
Divyank Shah <divyank.shah.2016@gmail.com>
2022-12-26 02:53:23 -0800

    general cleanup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,19 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import React from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import { Lexend, Press_Start_2P } from "@next/font/google";
+
+const lexend = Lexend({ subsets: ["latin"], variable: "--font-lexend" });
+const pixel = Press_Start_2P({
+  subsets: ["latin"],
+  variable: "--font-pixel",
+  weight: "400",
+});
+
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <main className={`${lexend.variable} ${pixel.variable}`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
