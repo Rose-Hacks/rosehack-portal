47d5681501a3fef95327dca736639f5fc3a0398b
Divyank Shah <divyank.shah.2016@gmail.com>
2023-01-01 14:12:07 -0800

    cleanup

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -31,40 +31,22 @@ const Navigation = () => {
         <Navbar.Collapse className="flex justify-center items-center">
           <Nav className="font-pixel flex lg:w-6/12 w-full justify-center items-center">
             <Nav.Link
+              href="/dashboard"
+              className="!text-base text-center text-white p-1 mx-2 hover:underline hover:drop-shadow-pink decoration-4 underline-offset-6"
+            >
+              Dashboard
+            </Nav.Link>
+            <Nav.Link
+              href="/rsvp"
+              className="!text-base text-center text-white p-1 mx-2 hover:underline hover:drop-shadow-pink decoration-4 underline-offset-6"
+            >
+              RSVP
+            </Nav.Link>
+            <Nav.Link
               href="https://rosehack.com/"
               className="!text-base text-center text-white p-1 mx-2 hover:underline hover:drop-shadow-pink decoration-4 underline-offset-6"
             >
-              HOME
-            </Nav.Link>
-            <Nav.Link
-              href="https://rosehack.com/#about"
-              className="!text-base text-center text-white p-1 mx-2 hover:underline hover:drop-shadow-blue decoration-4 underline-offset-6"
-            >
-              ABOUT
-            </Nav.Link>
-            <Nav.Link
-              href="https://rosehack.com/#sponsors"
-              className="!text-base text-center text-white p-1 mx-2 hover:underline hover:drop-shadow-pink decoration-4 underline-offset-6"
-            >
-              SPONSORS
-            </Nav.Link>
-            <Nav.Link
-              href="https://rosehack.com/#board"
-              className="!text-base text-center text-white p-1 mx-2 hover:underline hover:drop-shadow-blue decoration-4 underline-offset-6"
-            >
-              TEAM
-            </Nav.Link>
-            <Nav.Link
-              href="https://rosehack.com/#faq"
-              className="!text-base text-center text-white p-1 mx-2 hover:underline hover:drop-shadow-pink decoration-4 underline-offset-6"
-            >
-              FAQ
-            </Nav.Link>
-            <Nav.Link
-              href="https://rosehack.com/register"
-              className="!text-base text-center text-white p-1 mx-2 hover:underline hover:drop-shadow-blue decoration-4 underline-offset-6"
-            >
-              REGISTER
+              Rosehack
             </Nav.Link>
             {login && (
               <Nav.Link
