5ba990f2bfc3e5856c786a01c0eccb20d4d3b736
Divyank Shah <divyank.shah.2016@gmail.com>
2023-01-07 16:44:56 -0800

    disable check

--- pages/api/getUser.ts
+++ pages/api/getUser.ts
@@ -13,7 +13,8 @@ export default async function addStudent(
     return;
   } catch {
     // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
-    const email =
+    const email: string =
+    // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
       req.body.email.charAt(0).toUpperCase() + req.body.email.slice(1);
     try {
       const snapshot = await getDoc(doc(db, "users", email));
