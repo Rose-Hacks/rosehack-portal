8f19fd2f339b842772c7b70353926ab448e484e1
Divyank Shah <divyank.shah.2016@gmail.com>
2023-01-07 16:46:20 -0800

    formatting

--- pages/api/getUser.ts
+++ pages/api/getUser.ts
@@ -14,7 +14,7 @@ export default async function addStudent(
   } catch {
     // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
     const email =
-    // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
+      // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
       req.body.email.charAt(0).toUpperCase() + req.body.email.slice(1);
     try {
       const snapshot = await getDoc(doc(db, "users", email));
